2dbcfef788a5e7092340f24d2470de883d5fa605
Meryem <mvkoksal@gmail.com>
2025-10-06 17:12:45 -0500

    Completed input processing for texteditor

--- src/main/java/edu/grinnell/csc207/texteditor/TextEditor.java
+++ src/main/java/edu/grinnell/csc207/texteditor/TextEditor.java
@@ -1,22 +1,62 @@
 package edu.grinnell.csc207.texteditor;
+import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
 
+import java.io.IOException;
+
+import com.googlecode.lanterna.TerminalPosition;
+import com.googlecode.lanterna.TextCharacter;
+import com.googlecode.lanterna.input.KeyType;
+import com.googlecode.lanterna.screen.Screen;
+import com.googlecode.lanterna.input.KeyStroke;
 /**
  * The driver for the TextEditor Application.
  */
+
+ 
+ // WIDTH 50
+ // HEIGHT 50
+ // row: cursorPos / 50
+ // column: cursorPos % 50
+
 public class TextEditor {
 
     /**
      * The main entry point for the TextEditor application.
      * @param args command-line arguments.
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         if (args.length != 1) {
             System.err.println("Usage: java TextEditor <filename>");
             System.exit(1);
         }
 
-        // TODO: fill me in with a text editor TUI!
         String path = args[0];
         System.out.format("Loading %s...\n", path);
+
+        GapBuffer buf = new GapBuffer();
+        Screen screen = new DefaultTerminalFactory().createScreen();
+        screen.startScreen();
+        
+        boolean isRunning = true;
+        while (isRunning) {
+            KeyStroke stroke = screen.readInput();
+            KeyType key = stroke.getKeyType();
+
+            if (key.equals (KeyType.Character)) {
+                buf.insert(stroke.getCharacter());
+            } else if (key.equals (KeyType.ArrowLeft)) {
+                buf.moveLeft();
+            } else if (key.equals (KeyType.ArrowRight)) {
+                buf.moveRight();
+            } else if (key.equals (KeyType.Backspace)) {
+                buf.delete();
+            } else if (key.equals (KeyType.Escape)) {
+                screen.stopScreen();
+                isRunning = false;
+                System.exit(1);
+            }
+            // drawBuffer(buf, screen);
+        }
+
     }
 }
