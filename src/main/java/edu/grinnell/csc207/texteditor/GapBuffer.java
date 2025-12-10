8a8b8d7fb6ba6fc5bb4c9b0773e4d016ae1ef9f3
Meryem <mvkoksal@gmail.com>
2025-12-10 00:08:07 -0600

    Fixed final bugs

--- src/main/java/edu/grinnell/csc207/texteditor/GapBuffer.java
+++ src/main/java/edu/grinnell/csc207/texteditor/GapBuffer.java
@@ -4,6 +4,7 @@ package edu.grinnell.csc207.texteditor;
  * A gap buffer-based implementation of a text buffer.
  */
 public class GapBuffer {
+
     public char[] array;
     int gapBeg;
     int gapEnd;
@@ -17,33 +18,42 @@ public class GapBuffer {
         this.gapEnd = 0;
         array = new char[0];
     }
-
+    
     /**
      * Inserts a character into the buffer
-     * @param ch a character to insert
+     * @param ch, a character to insert
      */
     public void insert(char ch) {
-        if (gapEnd - gapBeg == 0) {
-            char[] newArray = new char[array.length * 2];
-            // copy the left part
-            for (int i = 0; i < gapBeg; i++) {
-                newArray[i] = array[i];
+        // If the array is empty, initialize it and add ch
+        if (array.length == 0) {
+            array = new char[3];
+            gapBeg = 1;
+            gapEnd = array.length;
+            array[0] = ch;
+        // If the array is full, grow the buffer
+        } else {
+            if (gapEnd - gapBeg == 0) {
+                char[] newArray = new char[array.length*2];
+                // copy the left part
+                for (int i = 0; i < gapBeg; i++) {
+                    newArray[i] = array[i];
+                }
+                // variable counting the number of elements in the right section
+                int afterCursorCopyNum = array.length - gapEnd;
+                // copy the right part
+                for (int i = 1; i <= afterCursorCopyNum; i++) {
+                    newArray[newArray.length - i] = array[array.length - i];
+                }
+                    // move gapEnd cursor
+                    gapEnd = newArray.length - afterCursorCopyNum; 
+                    array = newArray;
             }
-            // variable counting the number of elements in the right section
-            int afterCursorCopyNum = array.length - gapEnd;
-            // copy the right part
-            for (int i = 1; i <= afterCursorCopyNum; i++) {
-                newArray[newArray.length - i] = array[array.length - i];
-            }
-            // move gapEnd cursor
-            gapEnd = newArray.length - afterCursorCopyNum; 
-            array = newArray;
-        }
-        // add new ch
-        array[gapBeg] = ch;
+            // add new ch
+            array[gapBeg] = ch;
 
-        // move gapBeg cursor
-        gapBeg++;
+            // move gapBeg cursor
+            gapBeg++;
+        }
     }
 
     /**
